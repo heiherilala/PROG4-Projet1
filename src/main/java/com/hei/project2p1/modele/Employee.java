09a37814ceb7ff6380225a821c0f3a4a73e4c076
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-07-14 07:40:41 +0300

    feat: add unique constraint

--- src/main/java/com/hei/project2p1/modele/Employee.java
+++ src/main/java/com/hei/project2p1/modele/Employee.java
@@ -9,6 +9,10 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity(name = "employee")
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "unique_registration_No",
+        columnNames = { "registrationNo" })
+})
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,11 +21,12 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    //or @Column(unique=true) use on creation of db
     private String registrationNo;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
     @Lob
-    @Column//(columnDefinition = "varchar(max)")
+    @Column //(columnDefinition = "varchar(max)")?
     private String photo;
 }
