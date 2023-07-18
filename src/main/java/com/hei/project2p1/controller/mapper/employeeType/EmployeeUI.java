68342b5997fb9f2c8a959a73f84ad3e5b6fc8495
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-07-18 09:59:42 +0300

    feat: add 14 new attributs to EmployeeUI

--- src/main/java/com/hei/project2p1/controller/mapper/employeeType/EmployeeUI.java
+++ src/main/java/com/hei/project2p1/controller/mapper/employeeType/EmployeeUI.java
@@ -1,7 +1,10 @@
 package com.hei.project2p1.controller.mapper.employeeType;
 
+import com.hei.project2p1.modele.Phone;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 
 @Data
@@ -13,4 +16,18 @@ public class EmployeeUI {
     private String lastName;
     private String birthDate;
     private String photo;
+    private String gender;
+    private List<Phone> phones;
+    private String address;
+    private String personalEmail;
+    private String professionalEmail;
+    private String cinNumber;
+    private String cinIssueDate;
+    private String cinIssuePlace;
+    private String function;
+    private Integer numberOfChildren;
+    private String hiringDate;
+    private String departureDate;
+    private String socioProfessionalCategory;
+    private String cnapsNumber;
 }
