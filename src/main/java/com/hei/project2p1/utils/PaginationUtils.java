f0ca9bc6a99a66efbce702e9fcafdb5d6d988315
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-07-22 22:17:48 +0300

    feat: pagination validation

---
+++ src/main/java/com/hei/project2p1/utils/PaginationUtils.java
@@ -0,0 +1,20 @@
+package com.hei.project2p1.utils;
+
+import com.hei.project2p1.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaginationUtils {
+    public final static int PAGE_SIZE_MAX = 200;
+    private PaginationUtils(){
+    }
+
+    public static void paginationValidator(int page, int pageSize){
+        if(page<1){
+            throw new BadRequestException("page must be >=1");
+        }
+        if(pageSize>PAGE_SIZE_MAX){
+            throw new BadRequestException("page size too large, must be less than "+PAGE_SIZE_MAX);
+        }
+    }
+}
