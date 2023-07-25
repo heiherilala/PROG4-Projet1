d015dc4e8b2cfc9674b813f0bd818087da44fbb4
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-07-25 23:18:38 +0300

    feat: object to csv converter

--- src/main/java/com/hei/project2p1/utils/ObjectToCSVConverter.java
+++ src/main/java/com/hei/project2p1/utils/ObjectToCSVConverter.java
@@ -1,2 +1,36 @@
-package com.hei.project2p1.utils;public class ObjectToCSVConverter {
+package com.hei.project2p1.utils;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+public class ObjectToCSVConverter {
+
+    public static String convertToCSV(Object object) {
+        Class<?> clazz = object.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        List<Field> fieldList = Arrays.asList(fields);
+
+        StringBuilder csvData = new StringBuilder();
+
+        // Append headers
+        fieldList.forEach(field -> csvData.append(field.getName()).append(","));
+
+        csvData.deleteCharAt(csvData.length() - 1);
+        csvData.append("\n");
+
+        // Append values
+        fieldList.forEach(field -> {
+            field.setAccessible(true);
+            try {
+                csvData.append(field.get(object)).append(",");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+
+        csvData.deleteCharAt(csvData.length() - 1);
+
+        return csvData.toString();
+    }
 }
