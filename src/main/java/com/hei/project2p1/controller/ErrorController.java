fab7a73b09b2bb84544987ffd6602c87c17e0fd3
heiherilala <hei.herilala@gmail.com>
2023-08-23 13:57:28 +0300

    fix:  error page

--- src/main/java/com/hei/project2p1/controller/ErrorController.java
+++ src/main/java/com/hei/project2p1/controller/ErrorController.java
@@ -1,9 +1,11 @@
 package com.hei.project2p1.controller;
 
 import com.hei.project2p1.service.SpringSessionService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.rmi.ServerException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.ui.Model;
@@ -12,10 +14,12 @@ import org.springframework.ui.Model;
 @AllArgsConstructor
 public class ErrorController {
 
-//    @ExceptionHandler(ServerException.class)
-//    public String handleBadRequestException(ServerException e, Model model) {
-//        model.addAttribute("exception", e);
-//        return "error";
-//    }
+    @ExceptionHandler(ServerException.class)
+    public String handleBadRequestException(ServerException e, HttpServletResponse response, Model model) {
+        int statusCode = response.getStatus();
+        model.addAttribute("exception", e);
+        model.addAttribute("status", statusCode);
+        return "error";
+    }
 
 }
